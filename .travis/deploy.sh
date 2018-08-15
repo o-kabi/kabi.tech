025c903842b3a347a6a844bd71fc53fc674ecc46
Oliver Kabi <O.kabi@outlook.com>
2018-08-16 00:17:20 +0100

    removed set command incase this is causing error 127 also removed pointless if

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,30 +1,20 @@
-
-#!/bin/bash
-
+#!/bin/sh
 # print outputs and exit on first failure
-echo "test0"
-set -xe
 echo "test1"
-if [ $TRAVIS_BRANCH == "master" ] ; then
-
-    # setup ssh agent, git config and remote
-    eval "$(ssh-agent -s)"
-    echo "test2"
-    ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "travis@kabi.tech:/var/www/kabi.tech"
-    git config user.name "Travis CI"
-    git config user.email "travis@kabi.tech"
-    echo "test3"
-    # commit compressed files and push it to remote
-    rm -f .gitignore
-    cp .travis/deployignore .gitignore
-    echo "test4"
-    git add .
-    git status # debug
-    git commit -m "Deploy compressed files"
-    git push -f deploy HEAD:master
-    echo "test5"
-else
-
-    echo "No deploy script for branch '$TRAVIS_BRANCH'"
-fi
+# setup ssh agent, git config and remote
+eval "$(ssh-agent -s)"
+echo "test2"
+ssh-add ~/.ssh/travis_rsa
+git remote add deploy "travis@kabi.tech:/var/www/kabi.tech"
+git config user.name "Travis CI"
+git config user.email "travis@kabi.tech"
+echo "test3"
+# commit compressed files and push it to remote
+rm -f .gitignore
+cp .travis/deployignore .gitignore
+echo "test4"
+git add .
+git status # debug
+git commit -m "Deploy compressed files"
+git push -f deploy HEAD:master
+echo "test5"
