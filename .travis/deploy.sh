5b36b96a794d2afedde24c071f17f09c060057b9
Oliver Kabi <O.kabi@outlook.com>
2018-08-16 21:18:17 +0100

    as per last

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,7 @@
 # print outputs and exit on first failure
 # setup ssh agent, git config and remote
 set -xe
-eval $(ssh-agent sh)
+eval $(ssh-agent bash)
 ssh-add /home/travis/.ssh/travis_rsa
 git remote add deploy "travis@kabi.tech:/var/www/kabi.tech"
 git config user.name "Travis CI"
